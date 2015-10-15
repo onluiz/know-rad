d033ef8e5819b409ca89d0a749c2a925a21193b7
onluiz <luiz.alberto1723@gmail.com>
2015-10-15 11:58:50 -0300

    ajustes

---
+++ knowrad/knowrad-importer/src/test/java/org/knowrad/MainTest.java
@@ -0,0 +1,16 @@
+package org.knowrad;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		
+		System.out.println("just a test");
+		
+	}
+	
+}
